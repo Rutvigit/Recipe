fa81f0c3210f3d3e0dbe3c9344253f1571a2b80c
Rutviptl <rutvipatel2503@gmail.com>
2024-11-06 22:35:43 +0530

    first commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,8 @@ import HeroSection from "../components/HeroSection";
 import Improveskills from "../components/Improveskills";
 import QuoteSection from "../components/QuoteSection";
 import ChefsSection from "../components/ChefsSection";
+
+
 export default function Home(){
     return(
        <div>
